e91f50adf014856859caf366b1a41e755c7018de
kisoritech <melijuba30@gmail.com>
2025-06-24 22:17:50 -0400

    Atualização

--- models/db.js
+++ models/db.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // ← IMPORTANTE!
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME
 });
 
 module.exports = db;
